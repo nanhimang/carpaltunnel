1b5e77b91a9740c58c8b7b4506d446632d9d376d
donring <nanhimang@naver.com>
2023-05-15 16:25:02 +0900

    Add files via upload
    gif function fully developed

---
+++ CPS_result+이름입력_v1.5.js
@@ -0,0 +1,188 @@
+// _변수명 정의     
+//#region 
+let wwpr = document.getElementsByClassName('whole_wrapper')[0]; // 검은색 배경
+let bg = document.getElementsByTagName('body')[0]; // 검은색 배경
+let dsc = document.querySelector('.desc.f'); 
+let dsc_mk = document.querySelector('.desc.f>.marked'); // 마무리 알림
+let dsc_p = document.getElementsByClassName('twinkle')[0]; // 랭킹 유도 멘트
+
+    // =result page
+let rp = document.getElementsByClassName('report')[0];
+let cp = document.getElementById('cps'); // click per second
+let rtr = document.getElementById('retry'); //retry
+let dwn = document.getElementById('down'); //retry
+let shre = document.getElementById('share'); //retry
+
+    // =input page
+let ip = document.getElementsByClassName('input')[0];
+let dsc2 = document.querySelector('.desc.s'); 
+let dsc2_mk = document.querySelector('.desc.s>.marked'); // 마무리 알림
+let dsc2_ht1 = document.querySelectorAll('.hint')[0]; 
+let dsc2_ht2 = document.querySelectorAll('.hint')[1]; 
+let dsc2_ht3 = document.querySelectorAll('.hint')[2]; 
+let user_nm = document.getElementById('name');
+let mn = document.getElementsByClassName('whole_pad')[0];
+let btp_back = document.querySelector('.pad.side'); //back to scroe
+let btp_reset = document.querySelectorAll('.pad.side')[1]; //reset
+//#endregion
+
+function copyToClipboard() {
+    navigator.clipboard.writeText('https://nanhimang.github.io/carpaltunnel/CPS_%EA%B2%8C%EC%9E%84_v1.6.html')
+    alert('URL has been copied to the clipboard!');
+}
+
+//.로컬 데이터 불러오기
+function displaying(){
+    var blobDataURL = localStorage.getItem('gif');
+    var el = document.getElementById("target");
+      el.href= blobDataURL;
+      el.download="CarpalTunnelGame.gif";
+      el.click();
+    setTimeout(()=>{
+        localStorage.removeItem('gif');   
+    },10000);
+  }
+
+// .쿠키 불러오기
+function getCookie(key) {
+    try{
+        const value = '; ' + document.cookie;
+        const targetValue = value.split('; ' + key + '=');
+        if (targetValue.length === 2) {
+          return (targetValue.pop().split(';').shift());
+        }
+        else{return targetValue;}
+    }
+    catch(err) {
+        alert(err.message); 
+      }
+  }
+
+cp.innerText=getCookie('score');
+setTimeout(page_opening,1500);
+
+//  .종료 화면 
+//#region
+function color_setTimeout(ii){ //글자 반짝반짝
+    setTimeout(()=>
+    {if (dsc_p.style.color=="rgb(57, 168, 242)"){
+        dsc_p.style.color="gray";}
+    else{
+        dsc_p.style.color="#39a8f2";}
+    }, ii*500)
+}
+
+function page_opening(){ // 반짝반짝
+    dsc_p.style.display="block";
+    var i=0;
+    /* ★. while은 조건문만 들어감 */
+    while(i<10){
+        color_setTimeout(i);
+        i++;            
+    }
+}
+    //  _retry 누를 때 event 
+rtr.addEventListener('click',function(){ //확인 창 누르면 새로고침
+    if(confirm("Try again?")){
+        location.href="CPS_게임_v1.7.html";
+    }
+})
+//#endregion
+
+// .INPUT 화면연결 이벤트
+//#region
+function flip(){
+    mn.style.transform="rotateY(80deg)";
+}
+function reflip(){
+    mn.style.transform="rotateY(0deg)";
+}
+function page_transition(){
+    rp.addEventListener('mouseover',flip);
+    rp.addEventListener('mouseout',reflip);
+    rp.addEventListener('mousedown',function(){
+        rtr.style="display:none"; //컨텐츠 가림
+        dwn.style="display:none"; //컨텐츠 가림
+        shre.style="display:none"; //컨텐츠 가림
+        dsc.style="display:none";
+        dsc2.style="display:block"; //컨텐츠 보임
+        mn.style.transform="rotateY(180deg)";
+        btp_back.style.display="flex";
+        btp_reset.style.display="flex";
+        rp.removeEventListener('mouseover',flip);
+        rp.removeEventListener('mouseout',reflip);
+    });
+}
+function page_back(){
+    rp.addEventListener('mouseover',flip);
+    rp.addEventListener('mouseout',reflip);
+    rtr.style="display:block; font-size:4rem;"; //컨텐츠 가림
+    dwn.style="display:block; font-size:4rem;"; //컨텐츠 가림
+    shre.style="display:block; font-size:4rem;"; //컨텐츠 가림
+    dsc.style="display:block";
+    dsc2.style="display:none"; //컨텐츠 보임
+    btp_back.style="display:none";
+    btp_reset.style="display:none";
+    mn.style.transform="rotateY(0deg)";
+}
+    setTimeout(page_transition,1500);
+//#endregion
+
+// .INPUT 화면
+btp_back.addEventListener('click',function(){
+    page_back();
+})
+btp_reset.addEventListener('mousedown',function(){
+    ip.style.backfaceVisibility="visible";
+    mn.style.animation= "flipping .1s 5";
+    user_nm.value="";
+    dsc2_ht1.style.display="none";
+    dsc2_ht2.style.display="none";   
+    dsc2_ht3.style.display="none";  
+})
+btp_reset.addEventListener('mouseup',function(){
+    ip.style.backfaceVisibility="hidden";
+    mn.style.animation= "";
+    user_nm.value="";
+})
+
+
+
+ 
+// 입력값이 있을 경우 1번째 문장 제시
+user_nm.addEventListener('keyup',function(e){
+    if (user_nm.value.length==0){
+        dsc2_ht1.style.display="none";
+        dsc2_ht2.style.display="none";   
+        dsc2_ht3.style.display="none";   
+        if (e.code=="Enter"){
+            dsc2_ht3.style.display="block";
+            dsc2_ht3.style.color="red";
+        }
+    }
+    if (user_nm.value.length>0){
+        dsc2_ht1.style.display="block";
+        dsc2_ht2.style.display="none";
+        dsc2_ht3.style.display="none";   
+        if (e.code=="Enter"){
+            page_fadeout();
+        }
+    else if (user_nm.value.length>10){
+            dsc2_ht1.style.display="none";
+            dsc2_ht2.style.display="block";
+            dsc2_ht3.style.display="none";   
+        }
+    }
+});
+
+
+
+// .INPUT fadeout
+function page_fadeout(){
+    wwpr.style.opacity = "0";
+    bg.style.backgroundColor="black";
+
+    setTimeout(function(){
+        location.href="CPS_랭킹화면_v1.7.html";
+    },1000);
+}
---
+++ capture_7.js
@@ -0,0 +1,89 @@
+window.onload = function(){
+	// 캡쳐 버튼 클릭 이벤트 등록
+	document.getElementsByClassName("touch")[0].addEventListener("click", executing);
+}
+
+//.전체 함수 실행
+function executing(e){
+    document.getElementsByClassName("touch")[0].removeEventListener("click", executing);
+    var S=new Date;
+    timming= setInterval(()=>{
+      var numb = document.getElementById('counting');
+      numb.innerHTML='00:00:'+String(((new Date-S)/1e3).toFixed(3)).padStart(6,'0');    
+    });
+    screenshot(e);
+}
+
+
+//.스크린 캡쳐 실행
+    function screenshot(e){
+        var canvasArray = [];
+        var count = 0;
+        var interval = setInterval(function(){
+            html2canvas(document.body).then(
+                function(canvas) {
+                     var cWd = canvas.width;
+                     var cHgt = canvas.height;
+                    console.log("executed count :"+count);
+                    if (count == 100){
+                        if (canvasArray.length == 100){
+                            clearInterval(interval);
+                            console.log("clear interval!", canvasArray);
+                            gifRandering(canvasArray);
+                        }
+                        else{
+                            console.log("Still on pushing canvasArray...")
+                        }
+                    }
+                    else{
+                          var wd = document.body.clientWidth;
+                          var hgt = document.body.clientHeight;
+                          var wdG = cWd-wd;
+                          var hgtG = cHgt-hgt;
+                          var Wd = wd-wdG;
+                          var Hgt = hgt-hgtG;
+                          let img = canvas.getContext('2d',{ willReadFrequently: true }).getImageData(wdG, hgtG, Wd, Hgt);
+                          img.width = Wd;
+                          img.height = Hgt;
+                          let c = document.createElement("canvas");
+                          c.width = Wd;
+                          c.height = Hgt;
+                          c.getContext('2d',{ willReadFrequently: true }).putImageData(img, 0, 0);
+                          canvasArray.push(c);
+                          count++;
+                }
+        });
+    }, 100)
+}
+
+
+//.스크린 캡쳐 GIF 랜더링
+function gifRandering(canvasArray){
+  clearInterval(timming);
+  if (canvasArray.length==100){
+    var gif = new GIF({
+      workers: 2,
+      quality: 10
+    });
+    console.log("gif loaded!");
+    for (let k in canvasArray){
+        const canvasElem = canvasArray[k];
+        gif.addFrame(canvasElem,{delay:100});
+    }
+    gif.on('finished', function(blob) {
+      console.log("All frame added!");
+      console.log(blob);
+      const reader = new FileReader();
+      reader.onload = () => { 
+        const dataURL = reader.result;
+        localStorage.setItem('gif', dataURL);
+        console.log(dataURL);
+        setTimeout(()=>{
+          location.href = "CPS_result+이름입력_v1.4.html"
+        },1000);
+      }
+      reader.readAsDataURL(blob);
+  }, 'image/gif',0.5)
+    gif.render();      
+  }
+}    
